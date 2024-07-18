725aa247f5d5d0d2a3d18cb9d2f10f08d40eb87c
Andrei O <andrei@flashsoft.eu>
2024-07-18 23:32:21 +0300

    dev: 7/18/2024

--- src/lib/utils/server/crypto.ts
+++ src/lib/utils/server/crypto.ts
@@ -1,9 +1,10 @@
 export const getSha256 = async (password: string) => {
     let crypto
     if(typeof Deno !== 'undefined'){
-        crypto = (await import(`https://deno.land/std@0.170.0/node/crypto${'.ts'}`)).default
+        const hash = await window.crypto.subtle.digest('SHA-256', new TextEncoder().encode(password))
+        return btoa(String.fromCharCode(...new Uint8Array(hash)))
     } else {
         // crypto = await import('crypto')
     }
-    return crypto.createHash('sha256').update(password).digest('base64')
+    // return crypto.createHash('sha256').update(password).digest('base64')
 }
