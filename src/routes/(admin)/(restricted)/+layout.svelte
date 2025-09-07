c4ada8b121bfd2f2f1a2bdd71bcb8d22cf852a00
Andrei O. <andrei@flashsoft.eu>
2025-09-07 10:03:47 +0300

    chore: simplify favicon

--- src/routes/(admin)/(restricted)/+layout.svelte
+++ src/routes/(admin)/(restricted)/+layout.svelte
@@ -41,23 +41,11 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.24.2/jodit.es2018.min.css" />
 
   <!-- FavIcon  -->
-  <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-icon-57x57.png" />
-  <link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-icon-60x60.png" />
-  <link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-icon-72x72.png" />
-  <link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-icon-76x76.png" />
-  <link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-icon-114x114.png" />
-  <link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-icon-120x120.png" />
-  <link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-icon-144x144.png" />
-  <link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-icon-152x152.png" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-icon-180x180.png" />
-  <link rel="icon" type="image/png" sizes="192x192" href="/icon/android-icon-192x192.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
-  <link rel="manifest" href="/icon/manifest.json" />
-  <meta name="msapplication-TileColor" content="#ffffff" />
-  <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png" />
-  <meta name="theme-color" content="#ffffff" />
+  <link rel="icon" type="image/png" href="/icon/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/icon/favicon.svg" />
+  <link rel="shortcut icon" href="/icon/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
+  <link rel="manifest" href="/icon/site.webmanifest" />
   <!-- /FavIcon  -->
 </svelte:head>
 
--- vite.config.js
+++ vite.config.js
@@ -1,81 +1,62 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 // import { partytownVite } from '@builder.io/partytown/utils'
-import { join } from 'path'
+import { join } from 'path';
 import tailwindcss from '@tailwindcss/vite';
-
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		tailwindcss(),
-		// nodePolyfills({
-		// 	protocolImports: true
-		// }),
-		sveltekit(),
-		// partytownVite({
-		// 	// `dest` specifies where files are copied to in production
-		// 	dest: join(process.cwd(), 'build', '~partytown')
-		//   }),
-		SvelteKitPWA({
-			manifest: {
-				"$schema": "https://json.schemastore.org/web-manifest-combined.json",
-				"name": "Blog flashsoft.eu",
-				"short_name": "blog.flashsoft.eu",
-				"description": "PWA for domain blog.flashsoft.eu.",
-				"categories": ["programming", "personal", "blog", "tech", "dev"],
-				"orientation": "portrait-primary",
-				"icons": [
-					{
-						"src": "/images/icons/icon-512x512.png",
-						"sizes": "512x512",
-						"type": "image/png"
-					},
-					{
-						"src": "/images/icons/icon-384x384.png",
-						"sizes": "384x384",
-						"type": "image/png"
-					},
-					{
-						"src": "/images/icons/icon-192x192.png",
-						"sizes": "192x192",
-						"type": "image/png"
-					},
-					{
-						"src": "/images/icons/icon-152x152.png",
-						"sizes": "152x152",
-						"type": "image/png"
-					},
-					{
-						"src": "/images/icons/icon-128x128.png",
-						"sizes": "128x128",
-						"type": "image/png"
-					},
-					{
-						"src": "/images/icons/icon-96x96.png",
-						"sizes": "96x96",
-						"type": "image/png"
-					}
-				],
-				"theme_color": "#ffffff",
-				"background_color": "#ffffff",
-				"display": "standalone"
-			}
-		})
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	define: {
-		VERSION: JSON.stringify(process.env.npm_package_version),
-		SK_VERSION: JSON.stringify(process.env['npm_package_devDependencies__sveltejs_kit'])
-	},
-	server: {
-		port: 5512,
-		allowedHosts: ['tun-5512.flashsoft.eu'],
-		cors: true,
-		strictPort: true,	
-	}
+  plugins: [
+    tailwindcss(),
+    // nodePolyfills({
+    // 	protocolImports: true
+    // }),
+    sveltekit(),
+    // partytownVite({
+    // 	// `dest` specifies where files are copied to in production
+    // 	dest: join(process.cwd(), 'build', '~partytown')
+    //   }),
+    SvelteKitPWA({
+      manifest: {
+        $schema: 'https://json.schemastore.org/web-manifest-combined.json',
+        name: 'Blog flashsoft.eu',
+        short_name: 'blog.flashsoft.eu',
+        description: 'PWA for domain blog.flashsoft.eu.',
+        categories: ['programming', 'personal', 'blog', 'tech', 'dev'],
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: '/icon/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/icon/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone'
+      }
+    })
+  ],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  define: {
+    VERSION: JSON.stringify(process.env.npm_package_version),
+    SK_VERSION: JSON.stringify(process.env['npm_package_devDependencies__sveltejs_kit'])
+  },
+  server: {
+    port: 5512,
+    allowedHosts: ['tun-5512.flashsoft.eu'],
+    cors: true,
+    strictPort: true
+  }
 };
 
 export default config;
